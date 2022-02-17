69c511f96e9b1c16aee67a1e4509e52af2794c56
Jairo Tunisse <jairo_tunisse@outlook.com>
2022-02-17 10:37:26 -0300

    improviments: prismic connection added :heavy_check_mark:

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,8 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client';
 
 export default function Posts() {
     return (
@@ -7,7 +10,7 @@ export default function Posts() {
             <Head>
                 <title>Posts | Ignews</title>
             </Head>
-            <main className={styles.container}> 
+            <main className={styles.container}>
                 <div className={styles.posts}>
                     <a>
                         <time>16 de fevereiro de 2022</time>
@@ -37,5 +40,31 @@ export default function Posts() {
         </>
 
     );
+
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'publication')
+
+    ], {
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100,
+    }
+
+    )
+    console.log(JSON.stringify(response, null, 2));
+
+    return {
+        props: {
+
+
+        }
+
+    }
+
 
 }
